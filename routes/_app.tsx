f218ba7ae523e26f71093eabcd3f775b1b1ddf4c
JQ <jqocon@gmail.com>
2024-02-14 09:53:30 +0000

    feat: add favicon

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -4,15 +4,21 @@ export default function App({ Component }: PageProps) {
     <html>
       <head>
         <title>Delowcos Studio</title>
+        
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta
           name="description"
           content="Estudio de producción musical en Las Palmas de Gran Canaria"
         />
+
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
         <link rel="stylesheet" href="/styles.css" />
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.9.1/gsap.min.js">
-        </script>
+        
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.9.1/gsap.min.js" />
       </head>
       <body>
         <Component />
