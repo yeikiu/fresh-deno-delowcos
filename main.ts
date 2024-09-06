240f9dc339291428ba0142c6b69325a0b7823e61
JQ <jqocon@gmail.com>
2024-09-06 03:25:13 +0100

    revert: analytics

--- main.ts
+++ main.ts
@@ -9,11 +9,5 @@ import "$std/dotenv/load.ts";
 import { start } from "$fresh/server.ts";
 import manifest from "./fresh.gen.ts";
 import config from "./fresh.config.ts";
-import { gaPlugin } from "ga/mod.ts";
 
-await start(manifest, {
-    ...config,
-    plugins: [
-        gaPlugin()
-    ]
-});
+await start(manifest, config);
